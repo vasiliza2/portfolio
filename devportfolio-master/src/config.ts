d93bd4839de7763c5e2d2b2c39136607af9cd6de
Elizaveta <elizavetavasileva332@gmail.com>
2025-11-01 12:27:17 +0300

    3

--- devportfolio-master/src/config.ts
+++ devportfolio-master/src/config.ts
@@ -13,27 +13,69 @@ export const siteConfig = {
   skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
   projects: [
     {
-  name: "Название Проекта",
+  name: "1 курс",
+  links: [
+    { label: "Информатика", url: "https://drive.google.com/drive/u/0/folders/1oNxZqabOsJIMZU2ktY8vjNvRiDFtwAqF" },
+    { label: "ОКА", url: "https://drive.google.com/drive/u/0/folders/1oWgly20YSI-o0jSORGitIA48GFcUu29q" },
+    { label: "ИТФ", url: "https://drive.google.com/drive/u/0/folders/1oWgly20YSI-o0jSORGitIA48GFcUu29q" },
+    { label: "ИТ в математике", url: "https://drive.google.com/drive/u/0/folders/1CaFqRD0wBrMyNY8YVobv5xo0AeDIHMDH" },
+    { label: "Дискретная математика", url: "https://drive.google.com/drive/u/0/folders/1-ASMxIqosVHjQsXx11tyVq8NLgFpwp_O" },
+    { label: "Матанализ", url: "https://drive.google.com/drive/u/0/folders/1-BnvbO8FhwJskL_4iJrpnFMITsgLFIbl" },
+    { label: "ИТ", url: "https://drive.google.com/drive/u/0/folders/1aNC2DXCAYRu4E0NitnrfmrwLwgWZVfxf" },
+    { label: "ЛАТМ", url: "https://drive.google.com/drive/u/0/folders/12xfmWFiur0-m-8U5Qnu3zIW0FaycUvRM" },
+    { label: "Программирование СИ", url: "https://drive.google.com/drive/u/0/folders/1O1BfqUuvfPL-ms3YnjdjSg7cu9RaNO6m" },
+    { label: "Практика", url: "https://github.com/vasiliza2/practice1" },
+  ],
+},
+    {
+  name: "2 курс",
+  links: [
+    {label:"Компьютерный практикум"},
+    {label:"Лабораторная работа №2", url:"https://vasiliza2.github.io/vasiliza2/"},
+    {label:"Лабораторная работа №3", url:"https://docs.google.com/document/d/14M29aSf0uBgO-x2W78Gnqff0iRb6F4Q489aWuT1IEMQ/edit?usp=sharing"},
+    {label:"Лабораторная работа №4", url:"https://drive.google.com/file/d/15bT-9mkWPAMRW-Upu5EgAziYeYD8FARf/view?usp=sharing"},
+    {label:"Лабораторная работа №5", url:"https://docs.google.com/document/d/1rj9eo3ibbkc7SxvAc7x4gtAMJ8nsnnhey4bTA87ZtZA/edit?usp=sharing"},
+    {label:"Лабораторная работа №6", url:"https://docs.google.com/document/d/1GrUOMVz3bYZJCCVXT_We2zzyIhv-9l5OD3S2axb-y8g/edit?usp=sharing"},
+    {label:"Лабораторная работа №7", url:"https://docs.google.com/document/d/1Xiw_nqd9l0GyMbxBotyCgefhXKMv9WL5EGUjhV-wyUw/edit?usp=sharing"},
+    {label:"Лабораторная работа №8", url:"https://drive.google.com/file/d/1Z3FOXUNv6qqHaOvDMl6KKC_Q3tJN3aj9/view?usp=sharing"},
+    {label:"Лабораторная работа №9.1", url:"https://docs.google.com/document/d/1cvBTsSUzU_y2H-lC2LhdzmpM3dFbuwjfTuZlJX6d6sU/edit?usp=sharing"},
+    {label:"Лабораторная работа №9.2", url:"https://vasiliza2.github.io/lektor-kp/"},
+    {label:""},
+    { label: "СВТ", url: "https://drive.google.com/file/d/1zMeM0J3E8v4aUOvQ0WLZk21jqI0HHL03/view?usp=sharing"},
+    {label:""},
+    {label:"Веб-проектирование"},
+    {label:"Лабораторная работа №1. Портфолио", url:"https://vasiliza2.github.io/vasiliza2/"},
+    {label:"Лабораторная работа №2. Сторонние сервисы", url:"https://adamen-blog.vercel.app/post/0"},
+    {label:"Лабораторная работа №3. Пользовательские компоненты", url:"https://adamen-blog.vercel.app/post/1"},
+    {label:"Лабораторная работа №4. Строки", url:"https://drive.google.com/file/d/1SJjvbcnBenSlda_gzg4DfxBAjH91scAM/view?usp=sharing"},
+    {label:""},
+    { label: "Базы данных", url: "https://replit.com/@elizavetavasil1/portfolioBD#Readme.md" },
+    { label: "Программирование", url: "https://replit.com/@elizavetavasil1/portfolioprogha34siem" },
+    { label: "Интегралы и ДУ", url: "https://drive.google.com/drive/folders/1-rCEHRylhYaJHgj5b8qu6uQL3QhoR-R7?usp=sharing" },
+    { label: "Английский язык", url: "https://drihttps://drive.google.com/drive/folders/1-qpZQW18DjU1P7A0sRyLRJEYx4sWEVVA?usp=sharing" },
+    { label: "Пакеты прикладных программ", url: "https://drive.google.com/drive/folders/1DLhI2BLZyb5WqWV4Nw-5ifnkRXOoJXaG?usp=sharing" },
+    { label: "Анализ данных", url: "https://drive.google.com/drive/folders/1uJ7KiDKQPooMlkhQf0BYoURB6rSAcvB4?usp=sharing" },
+    { label: "ТКМ", url: "https://drive.google.com/drive/folders/1-f819WQe4iJOTcL_bYEQ5V-4i8SjHYLF?usp=sharing" },
+    { label: "Вычислительная математика", url: "https://drive.google.com/drive/folders/1KTmYfft9mQiZMjVzAahX3o9kruW4_vaa?usp=sharing" },
+    { label: "Практика", url: "https://drive.google.com/drive/folders/19CHi7ZLlkXy7ToLI3Y1KwY1K8kUybn7-?usp=sharing" },
+  ],
+},
+{
+  name: "3 курс",
   // ...
   links: [
     { label: "Ссылка 1", url: "https://url1.com" },
     { label: "Ссылка 2", url: "https://url2.com" },
   ],
 },
-    {
-      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
-      description:
-        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
-      link: "https://fullstackextensions.com/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
-    },
-    {
-      name: "ExtensionKit",
-      description:
-        "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
-      link: "https://extensionkit.io/?ref=devportfolio",
-      skills: ["React", "Node.js", "AWS"],
-    },
+{
+  name: "4 курс",
+  // ...
+  links: [
+    { label: "Ссылка 1", url: "https://url1.com" },
+    { label: "Ссылка 2", url: "https://url2.com" },
+  ],
+},
   ],
   experience: [
     {
